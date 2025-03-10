1da3a678f0b55b6f9faa6ab68340bcdca797684d
victor ugochukwu <nwitevictor7@gmail.com>
2025-03-10 19:05:50 +0100

    updated UI and layout

---
+++ libs/constants.ts
@@ -0,0 +1,8 @@
+export const APP_NAME = process.env.APP_NAME || "Solana DAPP";
+
+export const APP_SLOGAN =
+  process.env.APP_SLOGAN || "Seamless Web3 Access, Powered by Solana";
+
+export const APP_DESCRIPTION =
+  process.env.APP_DESCRIPTION ||
+  "NextJS + Solana + Phantom Wallet + Typescript";
