c4e1b46ccec32a2660e872158bbf0c0c9d2ff212
victor ugochukwu <nwitevictor7@gmail.com>
2025-03-14 23:12:08 +0100

    updated navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,7 +9,7 @@ export const Navbar = () => {
     <nav className="bg-[#000000] px-8 border-b border-[#1F2937]">
       <div className="flex justify-between items-center body-container h-16">
         <Link href={"/"} className="text-purple-700 text-2xl font-bold">
-          {APP_NAME}
+          {APP_NAME} 
         </Link>
 
         <div className="border hover:border-slate-900 rounded">
